38eed228ecad056641f6086e98d812f81a9f355b
Bipin <bipp2009@gmail.com>
2021-05-26 16:55:05 +0100

    modified schema and  seed files after MQSQL workbench testing

--- DB/schema.sql
+++ DB/schema.sql
@@ -1,6 +1,6 @@
 DROP DATABASE IF exists employee;
 
-CREATE employee;
+CREATE DATABASE employee;
 USE employee;
 
 CREATE table department(
@@ -9,7 +9,6 @@ id INT NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (id)
 );
 
-//create role table
 CREATE table role(
     id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
@@ -18,11 +17,11 @@ CREATE table role(
     PRIMARY KEY (id)
 );
 
-// create employee table
-CREATE table employee(
+CREATE table employee (
     id INT  NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
     manager_id INT,
+    PRIMARY KEY (id)
 );
