7ded545f19b56980484fe3034fc2520c8695f174
Bipin <bipp2009@gmail.com>
2021-07-07 16:29:13 +0100

    written first inquirer function and added DB functions

--- index.js
+++ index.js
@@ -19,4 +19,89 @@ const connection = mysql.createConnection({
 connection.connect((err) => {
   if (err) throw err;
   console.log("connected");
+  getUsersAction();
 });
+
+const getUsersAction = () => {
+  inquirer
+    .prompt({
+      name: 'action',
+      type: 'rawlist',
+      message: 'What would you like to do?',
+      choices: [
+          'Add department',
+          'Add role',
+          'Add employee',
+          'View departments',
+          'View roles',
+          'View employees',
+          'Update employee roles'
+      ]
+    })
+    .then ((answer) => {
+      switch (answer.action) {
+        case 'Add department':
+          addDepartment();
+          break;
+        case 'Add employee':
+          addEmployee();
+          break;
+        case 'Add role':
+          addRole();
+          break;
+        case 'View departments':
+          ViewDepartments();
+          break;
+          case 'View roles':
+            Viewroles();
+            break;
+            case 'View employees':
+              Viewemployees();
+              break;
+        case 'Update employee roles':
+          Updateemployeeroles();
+          break;  
+          default:
+            console.log(`Invalid action: ${answer.action}`);
+          break;   
+      }
+    })
+}
+//activiy 17 connection queries
+const addDepartment = () => {
+  console.log("User clicked add department"); 
+
+  getUsersAction();
+}
+
+const addEmployee = () => {
+  console.log("User clicked add employee");
+
+  getUsersAction();
+}
+
+const addRole = () => {
+  console.log("user clicked add role");
+  getUsersAction();
+}
+
+const ViewDepartments = () => {
+  console.log("user clicked view department");
+  getUsersAction();
+}
+
+const Viewroles = () => {
+  console.log("user clicked view roles");
+  getUsersAction();
+}
+
+const Viewemployees = () => {
+  console.log("user clicked view employees");
+  getUsersAction();
+}
+
+const Updateemployeeroles = () => {
+  console.log("user clicked update employee roles");
+  getUsersAction;
+}
+
